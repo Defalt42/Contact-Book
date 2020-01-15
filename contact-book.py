ec84e5bf62160928671aa8b13b81d22332c5b88c
bgilbert33 <bgilbert33@gatech.edu>
2020-01-14 18:44:46 -0600

    Add initial code

---
+++ contact-book.py
@@ -0,0 +1,30 @@
+contacts = [
+    {
+        "id": 1
+        "first_name": "John",
+        "last_name": "Silver",
+        "email": "john.silver@gmail.com",
+        "cell": "512-991-5491" 
+    }
+]
+
+def add_contact(f_name, l_name, email, cell):
+    pass
+
+def delete_contact(id):
+    pass
+
+def update_name(id, f_name, l_name):
+    pass
+
+def update_email(id, email):
+    pass
+
+def update_cell(id, cell_number):
+    pass
+
+def get_all_contacts():
+    pass
+
+def get_contact(id):
+    pass
