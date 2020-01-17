baaedad246654e887208be8309f785d83b618e1a
defalt42 <gilbertb4375@gmail.com>
2020-01-17 16:21:19 -0600

    Update contact book to be a class structure

---
+++ src/ContactBook.py
@@ -0,0 +1,31 @@
+from Contact import Contact
+
+class ContactBook:
+
+    contacts = []
+
+    def add_contact():
+        print("Please enter contact information")
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        phone = input("Phone: ")
+        email = input("Email: ")
+
+        new_contact = Contact(first_name, last_name, phone, email)
+
+        contacts.append(new_contact)
+
+    def display_contacts():
+        for contact in contacts:
+            info = contact.get_info()
+
+            print("**********")
+            print(contact.get_name())
+            print("**********")
+
+            print("First name: " + contact.first_name)
+            print("Last name: " + contact.last_name)
+            print("Phone: " + contact.phone)
+            print("Email: " + contact.email)
+
+            print("============================")
