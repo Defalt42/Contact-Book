5e46bf72219cba1553152f13a25e2442edf9312a
defalt42 <gilbertb4375@gmail.com>
2020-01-17 16:42:56 -0600

    Create initial main menu to test class methods

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from ContactBook import ContactBook
+
+contact_book = new ContactBook()
+
+print("Welcome to PyContacts!")
+print("This is a command line contact book program that will help keep track of all your contacts. Here are some options to get started.")
+
+# TODO: Create main menu
+
+print("1) List contacts")
+print("2) Add a contact")
+print("3) Remove a contact")
+print("4) Update a contact")
+
+option = int(input("What would you like to do? (type a number)"))
+
+
