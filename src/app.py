bdef9280ea17cc4a56a73fec337d9ab45b61508e
defalt42 <gilbertb4375@gmail.com>
2020-01-22 21:24:52 -0600

    Connect list contacts menu item

--- src/app.py
+++ src/app.py
@@ -1,17 +1,30 @@
 from ContactBook import ContactBook
 
-contact_book = new ContactBook()
+contact_book = ContactBook()
 
 print("Welcome to PyContacts!")
 print("This is a command line contact book program that will help keep track of all your contacts. Here are some options to get started.")
 
 # TODO: Create main menu
 
-print("1) List contacts")
-print("2) Add a contact")
-print("3) Remove a contact")
-print("4) Update a contact")
+while True:
+    print("1) List contacts")
+    print("2) Add a contact")
+    print("3) Remove a contact")
+    print("4) Update a contact")
+    print("5) Quit")
 
-option = int(input("What would you like to do? (type a number)"))
+    option = int(input("What would you like to do? (type a number) >> "))
+
+    if option == 5:
+        break
+
+    if option == 1:
+        contact_book.display_contacts()
+
+
+
+
+
 
 
